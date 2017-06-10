7f0fa515bb46464597816ffd405ab88d6eb00445
colorfulcodes <jhilene@gmail.com>
2017-06-09 22:42:35 -0400

    additions

---
+++ website.js
@@ -0,0 +1,6 @@
+$('#example1').typeIt({
+     strings: ["Full-Stack Developer.", "Artist.", "Traveler", "Language Enthusiast"],
+     speed: 150,
+     breakLines: false,
+     autoStart: false
+});
